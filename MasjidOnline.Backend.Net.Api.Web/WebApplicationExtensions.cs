422ae107b3bd545d50d6dbd189548de058d73af7
Hery Prasojo <69847809+HeryPrasojo@users.noreply.github.com>
2024-11-12 18:36:56 +0700

    feature: update

---
+++ MasjidOnline.Backend.Net.Api.Web/WebApplicationExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace MasjidOnline.Backend.Net.Api.Web;
+
+public static class WebApplicationExtensions
+{
+    public static void MapEndpoint(this WebApplication webApplication)
+    {
+        webApplication.MapPost("/public/login");
+    }
+}
