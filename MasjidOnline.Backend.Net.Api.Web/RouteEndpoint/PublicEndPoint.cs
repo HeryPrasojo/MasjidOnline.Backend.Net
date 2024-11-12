4885cff3712c6682ae6a119940f1bf7a0ad09b03
Hery Prasojo <hery.prasojo@moduit.id>
2024-11-12 18:36:56 +0700

    feature: update

---
+++ MasjidOnline.Backend.Net.Api.Web/RouteEndpoint/PublicEndPoint.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace MasjidOnline.Backend.Net.Api.Web.RouteEndpoint;
+
+public static class PublicEndPoint
+{
+    public static async Task<LoginResponse> login(LoginRequest loginRequest)
+    {
+
+    }
+}
