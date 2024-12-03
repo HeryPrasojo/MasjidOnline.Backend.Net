683852eff53a15692077e0942a1492333e6ac8ac
Hery Prasojo <69847809+HeryPrasojo@users.noreply.github.com>
2024-12-03 18:16:24 +0700

    feature: initial

--- MasjidOnline.Backend.Business/Authentication/LoginBusiness.cs
+++ MasjidOnline.Backend.Business/Authentication/LoginBusiness.cs
@@ -1,8 +1,12 @@
-﻿using MasjidOnline.Backend.Business.Interface.Authentication;
+﻿using MasjidOnline.Backend.Api.Model.User;
+using MasjidOnline.Backend.Business.Interface.Authentication;
 
 namespace MasjidOnline.Backend.Business.Authentication;
 
 public class LoginBusiness : ILoginBusiness
 {
+    public void Register(RegisterRequest registerRequest)
+    {
 
+    }
 }
