2a034c54627327533b11dc10b3ae8679adc3127f
Hery Prasojo <69847809+HeryPrasojo@users.noreply.github.com>
2024-12-16 18:32:14 +0700

    feature: initial

---
+++ MasjidOnline.Service.Hash512/Hash512Service.cs
@@ -0,0 +1,11 @@
+﻿using MasjidOnline.Service.Hash512.Interface;
+
+namespace MasjidOnline.Service.Hash512;
+
+public class Hash512Service : IHash512Service
+{
+    public void hash()
+    {
+
+    }
+}
