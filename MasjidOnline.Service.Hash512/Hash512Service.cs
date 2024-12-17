4a6d10b3417ce111947abaab437317dffce03d45
Hery Prasojo <69847809+HeryPrasojo@users.noreply.github.com>
2024-12-17 11:14:21 +0700

    feature: initial

--- MasjidOnline.Service.Hash512/Hash512Service.cs
+++ MasjidOnline.Service.Hash512/Hash512Service.cs
@@ -1,12 +1,21 @@
-﻿using System.Security.Cryptography;
+﻿using System;
+using System.Security.Cryptography;
 using MasjidOnline.Service.Hash512.Interface;
 
 namespace MasjidOnline.Service.Hash512;
 
 public class Hash512Service : IHash512Service
 {
-    public void hashRandom()
+    private readonly Random _random = new();
+
+    public string HashRandom()
     {
-        SHA512.HashDataAsync()
+        var number = _random.NextInt64();
+
+        var bytes = BitConverter.GetBytes(number);
+
+        var digest = SHA512.HashData(bytes);
+
+        return Convert.ToBase64String(digest);
     }
 }
