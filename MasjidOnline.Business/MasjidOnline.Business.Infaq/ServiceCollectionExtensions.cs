d65b7ed63953dcdc8a8342b3e93c3c63e9662de4
Hery Prasojo <69847809+HeryPrasojo@users.noreply.github.com>
2025-03-26 17:33:24 +0700

    feature: initial

--- MasjidOnline.Business/MasjidOnline.Business.Infaq/ServiceCollectionExtensions.cs
+++ MasjidOnline.Business/MasjidOnline.Business.Infaq/ServiceCollectionExtensions.cs
@@ -6,38 +6,38 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddInfaqBusiness(this IServiceCollection services)
     {
-        services.AddSingleton<Business.Infaq.Interface.Expire.IAddBusiness, Business.Infaq.Expire.AddBusiness>();
-        services.AddSingleton<Business.Infaq.Interface.Expire.IApproveBusiness, Business.Infaq.Expire.ApproveBusiness>();
-        services.AddSingleton<Business.Infaq.Interface.Expire.ICancelBusiness, Business.Infaq.Expire.CancelBusiness>();
-        services.AddSingleton<Business.Infaq.Interface.Expire.IGetManyBusiness, Business.Infaq.Expire.GetManyBusiness>();
-        services.AddSingleton<Business.Infaq.Interface.Expire.IGetManyNewBusiness, Business.Infaq.Expire.GetManyNewBusiness>();
-        services.AddSingleton<Business.Infaq.Interface.Expire.IGetOneBusiness, Business.Infaq.Expire.GetOneBusiness>();
-        services.AddSingleton<Business.Infaq.Interface.Expire.IGetOneNewBusiness, Business.Infaq.Expire.GetOneNewBusiness>();
-        services.AddSingleton<Business.Infaq.Interface.Expire.IRejectBusiness, Business.Infaq.Expire.RejectBusiness>();
+        services.AddSingleton<Interface.Expire.IAddBusiness, Expire.AddBusiness>();
+        services.AddSingleton<Interface.Expire.IApproveBusiness, Expire.ApproveBusiness>();
+        services.AddSingleton<Interface.Expire.ICancelBusiness, Expire.CancelBusiness>();
+        services.AddSingleton<Interface.Expire.IGetManyBusiness, Expire.GetManyBusiness>();
+        services.AddSingleton<Interface.Expire.IGetManyNewBusiness, Expire.GetManyNewBusiness>();
+        services.AddSingleton<Interface.Expire.IGetOneBusiness, Expire.GetOneBusiness>();
+        services.AddSingleton<Interface.Expire.IGetOneNewBusiness, Expire.GetOneNewBusiness>();
+        services.AddSingleton<Interface.Expire.IRejectBusiness, Expire.RejectBusiness>();
 
-        services.AddSingleton<Business.Infaq.Interface.Infaq.IAddAnonymBusiness, Business.Infaq.Infaq.AddAnonymBusiness>();
-        services.AddSingleton<Business.Infaq.Interface.Infaq.IGetManyBusiness, Business.Infaq.Infaq.GetManyBusiness>();
-        services.AddSingleton<Business.Infaq.Interface.Infaq.IGetManyDueBusiness, Business.Infaq.Infaq.GetManyDueBusiness>();
-        services.AddSingleton<Business.Infaq.Interface.Infaq.IGetOneBusiness, Business.Infaq.Infaq.GetOneBusiness>();
-        services.AddSingleton<Business.Infaq.Interface.Infaq.IGetOneDueBusiness, Business.Infaq.Infaq.GetOneDueBusiness>();
+        services.AddSingleton<Interface.Infaq.IAddAnonymBusiness, Infaq.AddAnonymBusiness>();
+        services.AddSingleton<Interface.Infaq.IGetManyBusiness, Infaq.GetManyBusiness>();
+        services.AddSingleton<Interface.Infaq.IGetManyDueBusiness, Infaq.GetManyDueBusiness>();
+        services.AddSingleton<Interface.Infaq.IGetOneBusiness, Infaq.GetOneBusiness>();
+        services.AddSingleton<Interface.Infaq.IGetOneDueBusiness, Infaq.GetOneDueBusiness>();
 
-        services.AddSingleton<Business.Infaq.Interface.Success.IAddBusiness, Business.Infaq.Success.AddBusiness>();
-        services.AddSingleton<Business.Infaq.Interface.Success.IApproveBusiness, Business.Infaq.Success.ApproveBusiness>();
-        services.AddSingleton<Business.Infaq.Interface.Success.ICancelBusiness, Business.Infaq.Success.CancelBusiness>();
-        services.AddSingleton<Business.Infaq.Interface.Success.IGetManyBusiness, Business.Infaq.Success.GetManyBusiness>();
-        services.AddSingleton<Business.Infaq.Interface.Success.IGetManyNewBusiness, Business.Infaq.Success.GetManyNewBusiness>();
-        services.AddSingleton<Business.Infaq.Interface.Success.IGetOneBusiness, Business.Infaq.Success.GetOneBusiness>();
-        services.AddSingleton<Business.Infaq.Interface.Success.IGetOneNewBusiness, Business.Infaq.Success.GetOneNewBusiness>();
-        services.AddSingleton<Business.Infaq.Interface.Success.IRejectBusiness, Business.Infaq.Success.RejectBusiness>();
+        services.AddSingleton<Interface.Success.IAddBusiness, Success.AddBusiness>();
+        services.AddSingleton<Interface.Success.IApproveBusiness, Success.ApproveBusiness>();
+        services.AddSingleton<Interface.Success.ICancelBusiness, Success.CancelBusiness>();
+        services.AddSingleton<Interface.Success.IGetManyBusiness, Success.GetManyBusiness>();
+        services.AddSingleton<Interface.Success.IGetManyNewBusiness, Success.GetManyNewBusiness>();
+        services.AddSingleton<Interface.Success.IGetOneBusiness, Success.GetOneBusiness>();
+        services.AddSingleton<Interface.Success.IGetOneNewBusiness, Success.GetOneNewBusiness>();
+        services.AddSingleton<Interface.Success.IRejectBusiness, Success.RejectBusiness>();
 
-        services.AddSingleton<Business.Infaq.Interface.Void.IAddBusiness, Business.Infaq.Void.AddBusiness>();
-        services.AddSingleton<Business.Infaq.Interface.Void.IApproveBusiness, Business.Infaq.Void.ApproveBusiness>();
-        services.AddSingleton<Business.Infaq.Interface.Void.ICancelBusiness, Business.Infaq.Void.CancelBusiness>();
-        services.AddSingleton<Business.Infaq.Interface.Void.IGetManyBusiness, Business.Infaq.Void.GetManyBusiness>();
-        services.AddSingleton<Business.Infaq.Interface.Void.IGetManyNewBusiness, Business.Infaq.Void.GetManyNewBusiness>();
-        services.AddSingleton<Business.Infaq.Interface.Void.IGetOneBusiness, Business.Infaq.Void.GetOneBusiness>();
-        services.AddSingleton<Business.Infaq.Interface.Void.IGetOneNewBusiness, Business.Infaq.Void.GetOneNewBusiness>();
-        services.AddSingleton<Business.Infaq.Interface.Void.IRejectBusiness, Business.Infaq.Void.RejectBusiness>();
+        services.AddSingleton<Interface.Void.IAddBusiness, Void.AddBusiness>();
+        services.AddSingleton<Interface.Void.IApproveBusiness, Void.ApproveBusiness>();
+        services.AddSingleton<Interface.Void.ICancelBusiness, Void.CancelBusiness>();
+        services.AddSingleton<Interface.Void.IGetManyBusiness, Void.GetManyBusiness>();
+        services.AddSingleton<Interface.Void.IGetManyNewBusiness, Void.GetManyNewBusiness>();
+        services.AddSingleton<Interface.Void.IGetOneBusiness, Void.GetOneBusiness>();
+        services.AddSingleton<Interface.Void.IGetOneNewBusiness, Void.GetOneNewBusiness>();
+        services.AddSingleton<Interface.Void.IRejectBusiness, Void.RejectBusiness>();
 
         return services;
     }
--- MasjidOnline.Business/MasjidOnline.Business.User/ServiceCollectionExtensions.cs
+++ MasjidOnline.Business/MasjidOnline.Business.User/ServiceCollectionExtensions.cs
@@ -9,18 +9,18 @@ public static class ServiceCollectionExtensions
     {
         services.AddSingleton<IInitializerBusiness, InitializerBusiness>();
 
-        services.AddSingleton<Business.User.Interface.Internal.IAddBusiness, Business.User.Internal.AddBusiness>();
-        services.AddSingleton<Business.User.Interface.Internal.IApproveBusiness, Business.User.Internal.ApproveBusiness>();
-        services.AddSingleton<Business.User.Interface.Internal.ICancelBusiness, Business.User.Internal.CancelBusiness>();
-        services.AddSingleton<Business.User.Interface.Internal.IGetManyBusiness, Business.User.Internal.GetManyBusiness>();
-        services.AddSingleton<Business.User.Interface.Internal.IGetManyNewBusiness, Business.User.Internal.GetManyNewBusiness>();
-        services.AddSingleton<Business.User.Interface.Internal.IGetOneBusiness, Business.User.Internal.GetOneBusiness>();
-        services.AddSingleton<Business.User.Interface.Internal.IGetOneNewBusiness, Business.User.Internal.GetOneNewBusiness>();
-        services.AddSingleton<Business.User.Interface.Internal.IRejectBusiness, Business.User.Internal.RejectBusiness>();
+        services.AddSingleton<Interface.Internal.IAddBusiness, Internal.AddBusiness>();
+        services.AddSingleton<Interface.Internal.IApproveBusiness, Internal.ApproveBusiness>();
+        services.AddSingleton<Interface.Internal.ICancelBusiness, Internal.CancelBusiness>();
+        services.AddSingleton<Interface.Internal.IGetManyBusiness, Internal.GetManyBusiness>();
+        services.AddSingleton<Interface.Internal.IGetManyNewBusiness, Internal.GetManyNewBusiness>();
+        services.AddSingleton<Interface.Internal.IGetOneBusiness, Internal.GetOneBusiness>();
+        services.AddSingleton<Interface.Internal.IGetOneNewBusiness, Internal.GetOneNewBusiness>();
+        services.AddSingleton<Interface.Internal.IRejectBusiness, Internal.RejectBusiness>();
 
-        services.AddSingleton<Business.User.Interface.User.IAddRegisterBusiness, Business.User.User.AddRegisterBusiness>();
-        services.AddSingleton<Business.User.Interface.User.ILoginBusiness, Business.User.User.LoginBusiness>();
-        services.AddSingleton<Business.User.Interface.User.ISetPasswordBusiness, Business.User.User.SetPasswordBusiness>();
+        services.AddSingleton<Interface.User.IAddRegisterBusiness, User.AddRegisterBusiness>();
+        services.AddSingleton<Interface.User.ILoginBusiness, User.LoginBusiness>();
+        services.AddSingleton<Interface.User.ISetPasswordBusiness, User.SetPasswordBusiness>();
 
         return services;
     }
