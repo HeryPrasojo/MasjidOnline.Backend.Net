5772cfd5e2b11eadb17839de88375739c8dcd2b7
Hery Prasojo <69847809+HeryPrasojo@users.noreply.github.com>
2025-03-28 11:48:21 +0700

    feature: initial

--- MasjidOnline.Business/MasjidOnline.Business.User/ServiceCollectionExtensions.cs
+++ MasjidOnline.Business/MasjidOnline.Business.User/ServiceCollectionExtensions.cs
@@ -7,8 +7,7 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddUserBusiness(this IServiceCollection services)
     {
-        // todo change to transient and test
-        services.AddSingleton<IInitializerBusiness, InitializerBusiness>();
+        services.AddTransient<IInitializerBusiness, InitializerBusiness>();
 
         services.AddSingleton<Interface.Internal.IAddBusiness, Internal.AddBusiness>();
         services.AddSingleton<Interface.Internal.IApproveBusiness, Internal.ApproveBusiness>();
