2f482d4f1fc34450fbfb82190e88c0597d8843e3
Hery Prasojo <69847809+HeryPrasojo@users.noreply.github.com>
2025-06-16 18:16:14 +0700

    feature: initial

--- MasjidOnline.Service/MasjidOnline.Service.Localization/Strings/PaymentStatusService.cs
+++ MasjidOnline.Service/MasjidOnline.Service.Localization/Strings/PaymentStatusService.cs
@@ -1,11 +1,9 @@
 using System.Collections.Generic;
-using System.Globalization;
 using MasjidOnline.Service.Localization.Interface.Strings;
 
 namespace MasjidOnline.Service.Localization.Strings;
 
-// undone subclass
-public class PaymentStatusService : IPaymentStatusService
+public class PaymentStatusService : StringService, IPaymentStatusService
 {
     private readonly Dictionary<string, Dictionary<string, string>> _paymentStatus = new()
     {
@@ -18,25 +16,5 @@ public class PaymentStatusService : IPaymentStatusService
         },
     };
 
-    public string this[string key]
-    {
-        get
-        {
-            var getResult = _paymentStatus.TryGetValue(key, out var values);
-
-            if (!getResult || (values == default)) return key;
-
-
-            getResult = values.TryGetValue(CultureInfo.CurrentUICulture.NativeName, out var value);
-
-            if (!getResult || (value == default))
-            {
-                getResult = values.TryGetValue("en", out value);
-
-                if (!getResult || (value == default)) return key;
-            }
-
-            return value;
-        }
-    }
+    protected override Dictionary<string, Dictionary<string, string>> Dictionaries => _paymentStatus;
 }
--- MasjidOnline.Service/MasjidOnline.Service/Service.cs
+++ MasjidOnline.Service/MasjidOnline.Service/Service.cs
@@ -1,6 +1,7 @@
 using System.Net.Http;
 using MasjidOnline.Service.Captcha.ReCaptcha;
 using MasjidOnline.Service.Interface;
+using MasjidOnline.Service.Localization.Strings;
 using MasjidOnline.Service.Mail.Interface.Model;
 using Microsoft.Extensions.Options;
 
@@ -17,19 +18,19 @@ public class Service(
     private Cryptography.Interface.IEncryption128128Service? _encryption128128Service;
 
 
-    public Captcha.Interface.ICaptchaService Captcha => new CaptchaService(_httpClientFactory, _googleOptions);
+    public Captcha.Interface.ICaptchaService Captcha { get; } = new CaptchaService(_httpClientFactory, _googleOptions);
 
-    public Cryptography.Interface.IEncryption128128Service Encryption128128 => _encryption128128Service ??= new Cryptography.Encryption128128Service(_cryptographyOptions, _hash128Service);
+    public Cryptography.Interface.IEncryption128128Service Encryption128128 { get; } = _encryption128128Service ??= new Cryptography.Encryption128128Service(_cryptographyOptions, _hash128Service);
 
-    public FieldValidator.Interface.IFieldValidatorService FieldValidator => new FieldValidator.FieldValidatorService();
+    public FieldValidator.Interface.IFieldValidatorService FieldValidator { get; } = new FieldValidator.FieldValidatorService();
 
-    public File.Interface.IFileService File => new File.FileService();
+    public File.Interface.IFileService File { get; } = new File.FileService();
 
-    public Hash.Interface.IHash128Service Hash128 => _hash128Service;
+    public Hash.Interface.IHash128Service Hash128 { get; } = _hash128Service;
 
-    public Hash.Interface.IHash512Service Hash512 => new Hash.Hash512Service();
+    public Hash.Interface.IHash512Service Hash512 { get; } = new Hash.Hash512Service();
 
-    public Mail.Interface.IMailSenderService MailSender => new Mail.MailKit.SmtpMailSenderService(_mailOptions);
+    public Mail.Interface.IMailSenderService MailSender { get; } = new Mail.MailKit.SmtpMailSenderService(_mailOptions);
 
-    public Localization.Interface.ILocalizationService Localization => new Localization.LocalizationService(new Localization.LocalizationStringService());
+    public Localization.Interface.ILocalizationService Localization { get; } = new Localization.LocalizationService(new Localization.LocalizationStringService(new PaymentStatusService()));
 }
