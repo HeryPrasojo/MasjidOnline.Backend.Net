cc2a6e799122aca991b201d4b0530652afee245f
Hery Prasojo <69847809+HeryPrasojo@users.noreply.github.com>
2025-11-16 20:00:01 +0700

    feat: localized json response

--- MasjidOnline.Api.Web/Program.cs
+++ MasjidOnline.Api.Web/Program.cs
@@ -8,9 +8,11 @@ using MasjidOnline.Api.Web.Middleware;
 using MasjidOnline.Api.Web.WebApplicationExtension;
 using MasjidOnline.Business;
 using MasjidOnline.Business.Model.Options;
+using MasjidOnline.Business.Model.Responses;
 using MasjidOnline.Data;
 using MasjidOnline.Data.EntityFramework;
 using MasjidOnline.Data.EntityFramework.SqLite;
+using MasjidOnline.Library;
 using MasjidOnline.Service;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -19,6 +21,14 @@ using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
+var r = new Response()
+{
+    ResultCode = ResponseResultCode.DataMismatch,
+};
+var s = JsonSerializer.Serialize(r);
+Console.WriteLine(s);
+return;
 
 var webApplication = BuildApplication(args);
 
--- MasjidOnline.Library/JsonSerializer.cs
+++ MasjidOnline.Library/JsonSerializer.cs
@@ -1,5 +1,79 @@
+using System;
+using System.Reflection;
+using System.Text;
+
 namespace MasjidOnline.Library;
 
 public static class JsonSerializer
 {
+    public static string Serialize(object obj)
+    {
+        var stringBuilder = new StringBuilder();
+
+        stringBuilder.Append('{');
+
+        var type = obj.GetType();
+
+        var propertyInfos = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+        var propertySeparatorFlag = false;
+
+        foreach (var propertyInfo in propertyInfos)
+        {
+            var propertyValue = propertyInfo.GetValue(obj);
+
+            if (propertyValue is Enum)
+            {
+                var defaultValue = Activator.CreateInstance(propertyInfo.PropertyType);
+                var isEqualDefaultValue = propertyValue.Equals(defaultValue);
+                if (!isEqualDefaultValue)
+                {
+                    AppendPropertySeparator(stringBuilder, ref propertySeparatorFlag);
+
+                    AppendPropertyNameAndPairSeparator(stringBuilder, propertyInfo);
+
+                    stringBuilder.Append($"\"{propertyValue}\"");
+                }
+            }
+            else if (propertyValue != default)
+            {
+                AppendPropertySeparator(stringBuilder, ref propertySeparatorFlag);
+
+                AppendPropertyNameAndPairSeparator(stringBuilder, propertyInfo);
+
+                if (propertyInfo.PropertyType == typeof(string))
+                {
+                    stringBuilder.Append($"\"{propertyValue}\"");
+                }
+                else if (propertyInfo.PropertyType.IsPrimitive)
+                {
+                    stringBuilder.Append(propertyValue);
+                }
+                else
+                {
+                    stringBuilder.Append(Serialize(propertyValue));
+                }
+            }
+        }
+
+        stringBuilder.Append('}');
+
+        return stringBuilder.ToString();
+    }
+
+    private static void AppendPropertyNameAndPairSeparator(StringBuilder stringBuilder, PropertyInfo propertyInfo)
+    {
+        stringBuilder.Append('"');
+        stringBuilder.Append(char.ToLower(propertyInfo.Name[0]));
+        stringBuilder.Append(propertyInfo.Name[1..]);
+        stringBuilder.Append("\":");
+    }
+
+    private static void AppendPropertySeparator(StringBuilder stringBuilder, ref bool propertySeparatorFlag)
+    {
+        if (propertySeparatorFlag)
+            stringBuilder.Append(',');
+        else
+            propertySeparatorFlag = true;
+    }
 }
