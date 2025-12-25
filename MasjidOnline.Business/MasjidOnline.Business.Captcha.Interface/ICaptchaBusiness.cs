71a2395b747f61056cc63d20fbe03f1742bc41d2
Hery Prasojo <69847809+HeryPrasojo@users.noreply.github.com>
2025-12-25 16:20:32 +0700

    feat: register

--- MasjidOnline.Business/MasjidOnline.Business.Captcha.Interface/ICaptchaBusiness.cs
+++
@@ -1,7 +0,0 @@
-namespace MasjidOnline.Business.Captcha.Interface;
-
-public interface ICaptchaBusiness
-{
-    ICaptchaPassBusiness Pass { get; }
-    ICaptchaVerificationBusiness Verification { get; }
-}
--- MasjidOnline.Business/MasjidOnline.Business.Captcha.Interface/ICaptchaPassBusiness.cs
+++
@@ -1,8 +0,0 @@
-﻿using MasjidOnline.Business.Captcha.Interface.Pass;
-
-namespace MasjidOnline.Business.Captcha.Interface;
-
-public interface ICaptchaPassBusiness
-{
-    IGetStatusBusiness GetStatus { get; }
-}
