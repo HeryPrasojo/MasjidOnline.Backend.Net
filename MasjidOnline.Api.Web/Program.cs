cb21fd7d89f5de195b81b72ed365de6e57c95049
Hery Prasojo <69847809+HeryPrasojo@users.noreply.github.com>
2026-01-26 17:50:48 +0700

    feat: use reverse proxy

--- MasjidOnline.Api.Web/Program.cs
+++ MasjidOnline.Api.Web/Program.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Diagnostics;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using MasjidOnline.Api.Web;
@@ -92,12 +91,6 @@ static WebApplication BuildApplication(string[] args)
     #endregion
 
     webApplicationBuilder.Services.AddHostedServices();
-
-
-    if (Debugger.IsAttached)
-    {
-        webApplicationBuilder.WebHost.UseKestrelHttpsConfiguration();
-    }
 
 
     webApplicationBuilder.WebHost.UseShutdownTimeout(TimeSpan.FromSeconds(16));
