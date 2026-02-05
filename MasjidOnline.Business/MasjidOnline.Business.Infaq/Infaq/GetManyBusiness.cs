742aa3f2737271ac89074a46e7b5a9198dca9f61
Hery Prasojo <69847809+HeryPrasojo@users.noreply.github.com>
2026-02-05 17:35:13 +0700

    feat: infaq get many

--- MasjidOnline.Business/MasjidOnline.Business.Infaq/Infaq/GetManyBusiness.cs
+++ MasjidOnline.Business/MasjidOnline.Business.Infaq/Infaq/GetManyBusiness.cs
@@ -41,12 +41,12 @@ public class GetManyBusiness(IService _service) : IGetManyBusiness
                 RecordCount = _service.Localization[getManyResult.RecordCount, session.CultureInfo],
                 Records = getManyResult.Records.Select(e => new GetManyResponseRecord
                 {
-                    Amount = e.Amount,
-                    DateTime = e.DateTime,
+                    Amount = _service.Localization[e.Amount, session.CultureInfo],
+                    DateTime = _service.Localization[e.DateTime, session.CultureInfo, "yyyy MMM dd, HH:mm"],
                     Id = e.Id,
                     MunfiqName = e.MunfiqName,
-                    Status = Mapper.Mapper.Infaq.InfaqStatus[e.Status],
-                    PaymentType = Mapper.Mapper.Payment.PaymentType[e.PaymentType],
+                    Status = _service.Localization[Mapper.Mapper.Infaq.InfaqStatus[e.Status], session.CultureInfo],
+                    PaymentType = _service.Localization[Mapper.Mapper.Payment.PaymentType[e.PaymentType], session.CultureInfo],
                 }),
             }
         };
