e509a98f7ac52a78423268ddca0e8eaa81009016
Hery Prasojo <69847809+HeryPrasojo@users.noreply.github.com>
2026-06-30 17:29:49 +0700

    feat: donation status

--- MasjidOnline.Entity/Donation/DonationStatus.cs
+++ MasjidOnline.Entity/Donation/DonationStatus.cs
@@ -4,25 +4,13 @@ public enum DonationStatus
 {
     New = 2,
 
+    FailRequest = 4,
+    Fail = 6,
 
-    CancelRequest = 4,
-    Cancel = 6,
+    SuccessRequest = 8,
+    Success = 10,
 
-    ExpireRequest = 8,
-    Expire = 10,
-
-    FailRequest = 12,
-    Fail = 14,
-
-    RejectRequest = 16,
-    Reject = 18,
-
-
-    SuccessRequest = 20,
-    Success = 22,
-
-
-    RefundRequest = 24,
-    Refund = 26,
+    RefundRequest = 12,
+    Refund = 14,
 }
 
